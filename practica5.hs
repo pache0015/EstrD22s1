64dd527ee2e28de6663c63bfc3039adbb9506f3c
pache0015 <pache.facundo@gmail.com>
2022-05-04 14:21:38 -0300

    Se suben consignas de practica 5

---
+++ practica5.hs
@@ -0,0 +1,64 @@
+-- SET
+
+-- emptyS :: Set a
+
+-- addS :: Eq a => a -> Set a -> Set a
+
+-- belongs :: Eq a => a -> Set a -> Bool
+
+-- sizeS :: Eq a => Set a -> Int
+
+-- removeS :: Eq a => a -> Set a -> Set a
+
+-- unionS :: Eq a => Set a -> Set a -> Set a
+
+-- setToList :: Eq a => Set a -> [a]
+
+-- losQuePertenecen :: Eq a => [a] -> Set a -> [a]
+
+-- sinRepetidos :: Eq a => [a] -> [a]
+
+-- unirTodos :: Eq a => Tree (Set a) -> Set a
+
+-- Queue
+
+-- emptyQ :: Queue a
+
+-- isEmptyQ :: Queue a -> Bool
+
+-- queue :: a -> Queue a -> Queue a
+
+-- firstQ :: Queue a -> a
+
+-- dequeue :: Queue a -> Queue a
+
+-- lengthQ :: Queue a -> Int
+
+-- queueToList :: Queue a -> [a]
+
+-- unionQ :: Queue a -> Queue a -> Queue a
+
+
+-- Stack (pila)
+
+-- emptyS :: Stack a
+
+-- isEmptyS :: Stack a -> Bool
+
+-- push :: a -> Stack a -> Stack a
+
+-- top :: Stack a -> a
+
+-- pop :: Stack a -> Stack a
+
+-- lenS :: Stack a -> Int
+
+
+-- apilar :: [a] -> Stack a
+
+-- desapilar :: Stack a -> [a]
+
+-- insertarEnPos :: Int -> a -> Stack a -> Stack a
+
+
+-- Queue con dos listas
